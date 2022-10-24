870fbb7c28f7b325813b85d156eff46a5915aee2
Peter Cheung <peter@6uohk.com>
2022-10-24 17:11:40 +0100

    update.

---
+++ provider/README.md
@@ -0,0 +1,4 @@
+# Starting the server
+
+- `Redis` is required!
+- run `docker run -dt -p 6379:6379 redis` before starting the server
--- server/src/main/java/com/example/server/config/SessionConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.server.config;
-
-//@EnableRedisHttpSession
-//public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
-//}
