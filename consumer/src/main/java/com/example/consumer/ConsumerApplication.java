1ef3dba07e4577e3d5d510eb07a5ed3efbb781e2
Peter Cheung <peter@6uohk.com>
2023-01-05 02:31:11 +0000

    Disable HystrixDashboard (To Be Fixed, not compatible JAVA 17)

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,13 +3,12 @@ package com.example.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrix
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 public class ConsumerApplication {
 
     public static void main(String[] args) {
